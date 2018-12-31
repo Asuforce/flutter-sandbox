b564681eb6b5cccc93d9db94b49571153d28ab4b
asuforce <owata.sn@gmail.com>
2018-12-31 16:45:22 +0900

    Create RandomWordsState

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -3,6 +3,14 @@ import 'package:english_words/english_words.dart';
 
 void main() => runApp(MyApp());
 
+class RandomWordsState extends State<RandomWords> {
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+    return Text(wordPair.asPascalCase);
+  }
+}
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
